9e8f4e6fbaa500d7bf691d2393701ce024345249
ethirajank <101498739+ethirajank@users.noreply.github.com>
2022-03-15 11:15:30 +0530

    new.class

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,19 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+System.out.println("Greens tech");
+}
+private void companyId() {
+System.out.println("1234");
+}
+private void companyAddress() {
+System.out.println("OMR CHENNAI");
+}
+public static void main(String[] args) {
+	CompanyInfo s = new CompanyInfo();
+	s.companyName();
+	s.companyId();
+	s.companyAddress();
+}
+}
